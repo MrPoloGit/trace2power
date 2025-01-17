797892ea21771e90758d22b4fd032b82bea66eda
Krzysztof Boronski <kboronski@antmicro.com>
2025-01-17 17:53:49 +0100

    Add CI with two example projects
    Signed-off-by: Krzysztof Boronski <kboronski@antmicro.com>

---
+++ examples/tristate/tristate.v
@@ -0,0 +1,15 @@
+// Copyright (c) 2024-2025 Antmicro <www.antmicro.com>
+// SPDX-License-Identifier: Apache-2.0
+
+module tristate(
+  input wire clk,
+  input wire dir, ctrl,
+  output reg out,
+  inout wire z_state
+);
+  always @(posedge clk) begin
+    out <= dir ? ctrl : z_state;
+  end
+
+  assign z_state = dir ? ctrl : 1'bz;
+endmodule
